f3f642634a354c93887cfa27ffb9517e72903a28
Tanishq Mahendra Saini <52647176+tanishq1306@users.noreply.github.com>
2020-11-10 17:12:39 +0530

    add on

--- String/8_longest_palindromic_substring/longest_palindromic_substring.cpp
+++ String/8_longest_palindromic_substring/longest_palindromic_substring.cpp
@@ -1,12 +1,44 @@
 /**
  *    author:  tanishq 
- *    created: 08.11.2020
+ *    created: 10.11.2020
 **/
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
+// Time Complexity - O(n ^ 2)
+// Space Complexity - O(1)
 
-int dp[10005][10005];
+string longestPalindromicSubstring (string &str) {
+	int n = str.length();
+	int startIndx= 0, maxLen = 1;
+	
+	for (int i = 1; i < n; i++) { 
+        // Even length palindrome 
+        int l = i - 1, r = i; 
+        while (l >= 0 && r < n && str[l] == str[r]) { 
+            if (r - l + 1 > maxLen) { 
+                startIndx = l; 
+                maxLen = r - l + 1; 
+            } 
+            l--; 
+            r++; 
+        } 
+  
+        // Odd length palindrome
+        l = i - 1; 
+        r = i + 1; 
+        while (l >= 0 && r < n && str[l] == str[r]) { 
+            if (r - l + 1 > maxLen) { 
+                startIndx = l; 
+                maxLen = r - l + 1; 
+            } 
+            l--; 
+            r++; 
+        } 
+    } 
+	
+	return str.substr(startIndx, maxLen);
+}
 
 signed main() {
 	
@@ -14,53 +46,10 @@ signed main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 	
-	//int t;
-	//cin >> t;
+	string str;
+	cin >> str;
 	
-	//while (t--) {
-		//string str;
-		//cin >> str;
-		
-		//int n = str.size();
-		//int l = 0, maxLen = 1;
-		
-		//for (int gap = 0; gap < n; gap++) {
-			//// starting point of every diagonal (i, j)
-			//for (int i = 0, j = gap; j < n; i++, j++) {
-				//// base case string with single characters
-				//if (gap == 0) {
-					//dp[i][j] = 1;
-				//}
-				//// string with two charactes
-				//else if (gap == 1) {
-					//if (str[i] == str[j]) {
-						//dp[i][j] = 1;
-					//}
-					//else {
-						//dp[i][j] = 0;
-					//}
-				//}
-				//// check whether extremes are equal or not.
-				//else {
-					//// extremes are equal and middle part is also a 
-					//// palindrome a....a
-					//if (str[i] == str[j] && dp[i + 1][j - 1] == 1) {
-						//dp[i][j] = 1;
-					//}
-					//else {
-						//dp[i][j] = 0;
-					//}
-				//}
-				
-				//if (dp[i][j] == 1 && gap > maxLen) {
-					//l = i;
-					//maxLen = j;
-				//}
-			//}
-		//}
-		
-		//cout << str.substr (l, maxLen) << endl;
-	//}
+	cout << longestPalindromicSubstring (str); 
 	
     return 0;
 } 
--- String/9_longest_recurring_subsequence/longest_recurring_subsequence.cpp
+++ String/9_longest_recurring_subsequence/longest_recurring_subsequence.cpp
@@ -7,10 +7,30 @@
 using namespace std;
 
 // Variation of LCS problem 
-int longestRepeatingSubsequence (str, n) {
+// do watch : https://www.youtube.com/watch?v=hbTaCmQGqLg&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=30
+
+int dp[1001][1001];
+int longestRepeatingSubsequence (string &str, int n) {
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= n; j++) {
+			// dp initialization case
+			if (i == 0 || j == 0) {
+				dp[i][j] = 0;
+				continue;
+			}
+			
+			if (str[i - 1] == str[j - 1] && i != j) {
+				dp[i][j] = 1 + dp[i - 1][j - 1];
+			}
+			else {
+				dp[i][j] = max (dp[i][j - 1], dp[i - 1][j]);
+			}
+		}
+	}
 	
-	
+	return dp[n][n];
 }
+
 signed main() {
 	
 	ios_base::sync_with_stdio(false);
